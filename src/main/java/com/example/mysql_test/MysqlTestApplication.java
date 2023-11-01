8ad79ac80bd9eb022ec394b80f12068d49a38a2b
ilovesecu <ilovesecu@gmail.com>
2023-11-01 23:49:49 +0900

    adsf

--- src/main/java/com/example/mysql_test/MysqlTestApplication.java
+++ src/main/java/com/example/mysql_test/MysqlTestApplication.java
@@ -1,9 +1,11 @@
 package com.example.mysql_test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MysqlTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/mysql_test/controller/HelloController.java
+++ src/main/java/com/example/mysql_test/controller/HelloController.java
@@ -10,5 +10,5 @@ public class HelloController {
         return "hello#1";
     }
 
-    
+
 }
