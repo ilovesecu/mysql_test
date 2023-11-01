1b04d856554844a8cd8b22ddac51d798a8df6e63
ilovesecu <ilovesecu@gmail.com>
2023-11-01 23:47:29 +0900

    Initial commit

---
+++ src/main/java/com/example/mysql_test/MysqlTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysql_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlTestApplication.class, args);
+	}
+
+}
